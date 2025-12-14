b61640d9a255a9e2b231473ea5c77de63104e778
Daniel Rodríguez <darsmind03@gmail.com>
2025-12-14 11:51:16 +0000

    Las imágenes no cargan en la sección recién implementada.

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
